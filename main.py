ef6b655f6629ca0c540c74af8fd448dd4f0fcb5c
awuzi <yahia.lamri@outlook.fr>
2021-07-26 10:43:25 +0200

    fix execution interval

--- main.py
+++ main.py
@@ -12,13 +12,12 @@ MQTT_USERNAME = "GROUPE10"
 MQTT_PASSWORD = "14991799"
 # un ID différent par Node
 NODE_ID = ["12345678", "7654321", "987654543", "58381714", "13960537", "68191831", "54158491", "69607072", "51488201", "22361595", "24114793", "62061965", "73214383", "60052006", "94981317"]
-client = mqtt.Client("client")
-client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
-client.connect(MQTT_BROKER)
-
 
 def generateSensorValue(sensor_id: int, min_value: int, max_value: int):
     print("-------- MQTT Mise à jour du capteur %s --------" % sensor_id)
+    client = mqtt.Client("client")
+    client.username_pw_set(username=MQTT_USERNAME, password=MQTT_PASSWORD)
+    client.connect(MQTT_BROKER)
     for node in NODE_ID:
         MQTT_TOPIC = GROUPNAME + "/" + node + "/" + str(sensor_id)
         MQTT_MSG = json.dumps({
@@ -31,7 +30,7 @@ def generateSensorValue(sensor_id: int, min_value: int, max_value: int):
         })
         client.publish(MQTT_TOPIC, MQTT_MSG)
         # print("NODE_ID %s Timestamp : %s" % (node, int(time.time())))
-
+    client.disconnect()
 
 def run():
     while datetime.now().minute not in {0, 2, 4, 6, 8, 10, 12, 14, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58 }:
@@ -43,12 +42,11 @@ def run():
     generateSensorValue(107, 0, 150)
     generateSensorValue(121, 0, 200000)
     while True:
-        sleep(60 * 15)
+        sleep(120)
         generateSensorValue(112, 23, 29)
         generateSensorValue(114, 0, 100)
         generateSensorValue(122, 0, 50)
         generateSensorValue(107, 0, 150)
         generateSensorValue(121, 0, 200000)
-
 
 run()
